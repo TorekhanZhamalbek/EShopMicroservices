5bffd302bbbaadb616d3b94e59c3af031815f249
Торехан Жамалбек <torex.zam@gmail.com>
2024-05-15 09:50:00 +0500

    added product create endpoint

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+        where TCommand : ICommand<Unit>
+    {
+    }
+
+    public interface ICommandHandler<in TCommand, TResponce> : IRequestHandler<TCommand, TResponce>
+        where TCommand : ICommand<TResponce>
+        where TResponce : notnull
+    {
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQueryHandler<in TQuery, TResponce> : IRequestHandler<TQuery, TResponce>
+        where TQuery : IQuery<TResponce>
+        where TResponce : notnull
+    {
+    }
+}
