8bec39ab82693cfd4a4d7c0201d8415149fb1e3e
Торехан Жамалбек <torex.zam@gmail.com>
2024-05-30 13:51:09 +0500

    finished catalog microservice

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
@@ -2,9 +2,7 @@
 {
     public record CreateProductRequest(string Name, List<string> Category, string Description,
         string ImageFile, decimal Price);
-
     public record CreateProductResponse(Guid Id);
-
     public class CreateProductEndPoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
@@ -12,9 +10,11 @@
             app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
             {
                 var command = request.Adapt<CreateProductCommand>();
+
                 var result = await sender.Send(command);
 
                 var response = result.Adapt<CreateProductResponse>();
+
                 return Results.Created($"/products/{response.Id}", response);
             })
             .WithName("CreateProduct")
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -2,8 +2,18 @@
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, 
         string ImageFile, decimal Price) : ICommand<CreateProductResult>;
-
     public record CreateProductResult(Guid Id);
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(command => command.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(command => command.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(command => command.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(command => command.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
 
     internal class CreateProductCommandHandler(IDocumentSession session) 
         : ICommandHandler<CreateProductCommand, CreateProductResult>
